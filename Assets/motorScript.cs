ab5431de5292ed05f5f35ed0679e0d68e603f5dc
Settheworldonfireiii <ivanradkevich@yahoo.com>
2018-03-01 13:29:08 +0300

    скопировал первую часть автора туториала и попытался прикрутить ОпенСВ. Пока что ошибок не выдаёт, но но и результат тоже. Может быть, я не правильно запускаю(добавляю плагин в папку и просто запускаю Юнити)?

---
+++ Assets/motorScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class motorScript : MonoBehaviour {
+
+	public float power = 0.0f;
+
+	void FixedUpdate () {
+		GetComponent<Rigidbody> ().AddRelativeForce (0, power, 0);
+	}
+}
